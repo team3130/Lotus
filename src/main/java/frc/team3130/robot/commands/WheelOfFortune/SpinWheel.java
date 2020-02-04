312746d4827f57d243feddeafc639987de6407cd
ckugel <53205881+ckugel@users.noreply.github.com>
2020-02-04 16:02:10 -0600

    wheel of fortune break? or fix? moved from kawashiwi

--- src/main/java/frc/team3130/robot/commands/WheelOfFortune/SpinWheel.java
+++ src/main/java/frc/team3130/robot/commands/WheelOfFortune/SpinWheel.java
@@ -1,11 +1,15 @@
 package frc.team3130.robot.commands.WheelOfFortune;
 
-import java.util.Set;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.team3130.robot.OI;
+import frc.team3130.robot.RobotMap;
+import frc.team3130.robot.subsystems.Climber;
+import frc.team3130.robot.subsystems.ExampleSubsystem;
 import frc.team3130.robot.subsystems.WheelOfFortune;
+
+import java.util.Set;
 
 public class SpinWheel implements Command {
     private final Set<Subsystem> subsystems;
