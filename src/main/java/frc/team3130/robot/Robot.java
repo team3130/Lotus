17eabdadbf1c7ec2a1f15c5fe197a0ef296f735b
Eastan Giebler <kupa528@yahoo.com>
2020-02-14 23:42:15 -0600

    Linear flywheel interpolation instantiation

--- src/main/java/frc/team3130/robot/Robot.java
+++ src/main/java/frc/team3130/robot/Robot.java
@@ -11,6 +11,7 @@ import frc.team3130.robot.commands.Chassis.DefaultDrive;
 import frc.team3130.robot.commands.Turret.ManualTurretAim;
 import frc.team3130.robot.subsystems.*;
 import frc.team3130.robot.vision.Limelight;
+import frc.team3130.robot.vision.WheelSpeedCalculations;
 
 import static frc.team3130.robot.OI.driverGamepad;
 
@@ -47,6 +48,8 @@ public class Robot extends TimedRobot {
 
         //Instantiate Limelight interface
         Limelight.GetInstance();
+
+        WheelSpeedCalculations.GetInstance();
 
         //Register and instantiate subsystems (optionally with default commands)
         //Note: registerSubsystem is NOT needed if setDefaultCommand is used
