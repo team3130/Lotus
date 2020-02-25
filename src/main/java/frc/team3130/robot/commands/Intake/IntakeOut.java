8b44b4152033d38c46db9af21176c26aebeb68b1
Tomas Valdes <tvaldes60@gmail.com>
2020-02-24 20:09:19 -0600

    Changed intake speed

--- src/main/java/frc/team3130/robot/commands/Intake/IntakeOut.java
+++ src/main/java/frc/team3130/robot/commands/Intake/IntakeOut.java
@@ -18,7 +18,7 @@ public class IntakeOut implements Command {
      */
     @Override
     public void initialize() {
-        Intake.runIntake(-0.4);
+        Intake.runIntake(-0.7);
     }
 
     /**
