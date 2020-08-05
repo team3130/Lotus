4f06baa3805bd25f71c182be81fd8e1bcdece3c8
Tomas Valdes <tvaldes60@gmail.com>
2020-08-05 13:34:12 -0500

    Added senstitivty variable on smardash

--- src/main/java/frc/team3130/robot/commands/Chassis/DefaultDrive.java
+++ src/main/java/frc/team3130/robot/commands/Chassis/DefaultDrive.java
@@ -1,5 +1,8 @@
 package frc.team3130.robot.commands.Chassis;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.team3130.robot.OI;
@@ -10,6 +13,11 @@ import java.util.Set;
 
 public class DefaultDrive implements Command {
     private final Set<Subsystem> subsystems;
+    private ShuffleboardTab tab = Shuffleboard.getTab("Chassis");
+
+    private NetworkTableEntry driveSensitivity =
+            tab.add("sensitivity", 1.0)
+                    .getEntry();
 
     public DefaultDrive() {
         this.subsystems = Set.of(Chassis.getInstance());
@@ -29,7 +37,7 @@ public class DefaultDrive implements Command {
      */
     @Override
     public void execute() {
-        double moveSpeed = -OI.driverGamepad.getRawAxis(1); //joystick's y axis is inverted
+        double moveSpeed = -OI.driverGamepad.getRawAxis(1) * driveSensitivity.getDouble(1.0) ; //joystick's y axis is inverted
         if (!Chassis.isLowGear())
             moveSpeed *= RobotMap.kMaxHighGearDriveSpeed;
         double turnSpeed = OI.driverGamepad.getRawAxis(4) * RobotMap.kMaxHighGearDriveSpeed;
