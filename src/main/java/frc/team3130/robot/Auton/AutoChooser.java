2c0d839b8a811c16761400e57d2d35a2a07f8749
Caleb <calebkugel1@gmail.com>
2021-01-26 10:27:09 -0600

    Made enums and classes for each path
    the classes only contain the bare minimum and are all refactored off of DriveS

--- src/main/java/frc/team3130/robot/Auton/AutoChooser.java
+++ src/main/java/frc/team3130/robot/Auton/AutoChooser.java
@@ -14,10 +14,51 @@ public class AutoChooser {
         TRAVELING_TO_FOURTH_POINT,
     }
 
-    private DriveS m_driveS = null;
+    public enum BeforeBarrelRacing {
+        TRAVELING_TO_FIRST_POINT,
+        TRAVELING_TO_SECOND_POINT,
+        TRAVELING_TO_THIRD_POINT,
+    }
 
-    /**<p>Makes an array list for a driveS specific chooser</p>*/
+    public enum BeforeBouncePath {
+        TRAVELING_TO_FIRST_POINT,
+        TRAVELING_TO_SECOND_POINT,
+        TRAVELING_TO_THIRD_POINT,
+    }
+
+    public enum BeforeGalacticSearchA {
+        TRAVELING_TO_FIRST_POINT,
+        TRAVELING_TO_SECOND_POINT,
+        TRAVELING_TO_THIRD_POINT,
+    }
+
+    public enum BeforeGalacticSearchB {
+        TRAVELING_TO_FIRST_POINT,
+        TRAVELING_TO_SECOND_POINT,
+        TRAVELING_TO_THIRD_POINT,
+    }
+
+    public enum BeforeSlalomPath {
+        TRAVELING_TO_FIRST_POINT,
+        TRAVELING_TO_SECOND_POINT,
+        TRAVELING_TO_THIRD_POINT,
+    }
+
+    /**<p>making referance variables</p>*/
+    private DriveS m_driveS = null;
+    private BarrelRacing m_barrelRacing = null;
+    private BouncePath m_bouncePath = null;
+    private GalacticSearchA m_galacticSearchA = null;
+    private GalacticSearchB m_galacticSearchB = null;
+    private SlalomPath m_slalomPath = null;
+
+    /**<p>Makes an array list for a specific chooser</p>*/
     private SendableChooser<DriveS> m_driveS_Chooser;
+    private SendableChooser<DriveS> m_barrelRacing_Chooser;
+    private SendableChooser<DriveS> m_bouncePath_Chooser;
+    private SendableChooser<DriveS> m_galacticSearchA_Chooser;
+    private SendableChooser<DriveS> m_galacticSearchB_Chooser;
+    private SendableChooser<DriveS> m_slalomPath_Chooser;
 
     /** <p>Making an automatic option chooser</p> */
     private Optional<AutoOptionBase> autoOption_ = Optional.empty();
