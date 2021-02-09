1883d339942bb716faae28915721bcd7cd34c791
Devpacito <dev.awesome9@gmail.com>
2021-02-08 18:04:31 -0600

    did calculation for kHoodTicksPerDegree
    hopefully I did this right, I just copy-pasted the calculation from kTurretTicksPerDegree, but changed what I thought was the gear ratio to what CAD Caleb told me the gear ratio was

--- src/main/java/frc/team3130/robot/RobotMap.java
+++ src/main/java/frc/team3130/robot/RobotMap.java
@@ -112,7 +112,7 @@ public class RobotMap {
 
     //Hood
     public static int kHoodForward = 17669;//TODO: FIND Real Number
-    public static double kHoodTicksPerDegree; //= (kUseCompbot ? *put comp gear ratio here* : *put practice ratio here* ;)
+    public static double kHoodTicksPerDegree = (1.0 / 360.0) * 4096.0 * (2211.0 / 145.0); //= (kUseCompbot ? *put comp gear ratio here* : *put practice ratio here* ;)
 
     //Flywheel
     public static double kFlywheelMaxVoltage = 12.0;
