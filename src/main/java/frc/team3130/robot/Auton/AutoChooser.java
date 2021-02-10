3043a5da1d25a8d2aff462b3c408136d373e29d3
Caleb <calebkugel1@gmail.com>
2021-02-10 14:42:05 -0600

    Fixed a fix for trajectory

--- src/main/java/frc/team3130/robot/Auton/AutoChooser.java
+++ src/main/java/frc/team3130/robot/Auton/AutoChooser.java
@@ -44,7 +44,7 @@ public class AutoChooser {
         config.setKinematics(m_chassis.getmKinematics());
 
         // here to be default in case exception needs handling
-        Trajectory trajectory = new Trajectory((List<Trajectory.State>) this.GenerateTrajectory(this.paths[4]));
+        this.trajectory = new Trajectory((List<Trajectory.State>) this.GenerateTrajectory(this.paths[4]));
 
         try {
             System.out.println("THE PATH IS" + this.paths[this.number] + "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
