685340f94ce2b8ffac277067b31a1028c5905aa8
Devpacito <dev.awesome9@gmail.com>
2021-02-24 19:56:58 -0600

    Made try-catch loops in case constructor throws an exception
    If(robot start crying) {don't}

--- src/main/java/frc/team3130/robot/sensors/vision/PixyCam.java
+++ src/main/java/frc/team3130/robot/sensors/vision/PixyCam.java
@@ -1,5 +1,6 @@
 package frc.team3130.robot.sensors.vision;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import io.github.pseudoresonance.pixy2api.Pixy2;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
@@ -9,7 +10,8 @@ import java.util.ArrayList;
 
 public class PixyCam {
 
-    private final Pixy2 pixy;
+    private Pixy2 m_pixy;
+    private boolean isPixyConnected;
     private final int targetError = 10;
     private final int xC3 = 215; //x-value of point C3 (ball we look at for Path A red)
     private final int yC3 = 167;  //y-val of C3
@@ -18,27 +20,50 @@ public class PixyCam {
     //pixy cam res is 315 x 207 (horizontal x vertical)
 
     public PixyCam(Link link){
-        pixy = Pixy2.createInstance(link);
-        pixy.init();
+        try{
+        m_pixy = Pixy2.createInstance(link);
+        m_pixy.init();
+        isPixyConnected = true;
+        }
+        catch (Exception ex) {
+            //If connection fails log the error and fall back to encoder based angles.
+            String str_error = "Pixy didn't get constructed right. This is a [REDACTED] moment " + ex.getLocalizedMessage();
+            DriverStation.reportError(str_error, true);
+            isPixyConnected = false;
+        }
     }
 
     public PixyCam(Link link, int arg){
-        pixy = Pixy2.createInstance(link);
-        pixy.init(arg);
+        try{
+            m_pixy = Pixy2.createInstance(link);
+            m_pixy.init(arg);
+            isPixyConnected = true;
+        }
+        catch (Exception ex) {
+            //If connection fails log the error and fall back to encoder based angles.
+            String str_error = "Pixy didn't get constructed right. This is a [REDACTED] moment " + ex.getLocalizedMessage();
+            DriverStation.reportError(str_error, true);
+            isPixyConnected = false;
+        }
     }
 
     private boolean isBallHere(Block targetBlock, int xTarget, int yTarget){
-        if(targetBlock.getX() >= xTarget - targetError && targetBlock.getX() <= xTarget + targetError){
-            if(targetBlock.getY() >= yTarget - targetError && targetBlock.getY() <= yTarget + targetError){
-                return true;
+       if(isPixyConnected) {
+           if (targetBlock.getX() >= xTarget - targetError && targetBlock.getX() <= xTarget + targetError) {
+               if (targetBlock.getY() >= yTarget - targetError && targetBlock.getY() <= yTarget + targetError) {
+                   return true;
 
-            }
-        }
-        return false;
+               }
+           }
+           return false;
+       }
+       System.out.println("Pixy isn't connected. Returning false(isBallHere()) This is a [REDACTED] moment");
+       return false;
     }
     //path should be either "A" or "B" depending on the path
     Block largestBlock = largestBlock();
     public boolean isRedPath(String path){
+       if(isPixyConnected){
         if (path.equals("A")){
             if(isBallHere(largestBlock, xC3,yC3)){
                 return true;
@@ -55,18 +80,24 @@ public class PixyCam {
             System.out.println("Either: Neither A nor B was input for the method or my code broke. returning false (path blue) YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
             return false;
         }
+       }
+       else{
+           System.out.println("Pixy isn't connected. Returning false(isRedPath()) (blue has been set to path). This is a [REDACTED] moment");
+           return false;
+       }
 
     }
 
-    private Block largestBlock(){
+    private Block largestBlock() {
 
-       // System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
+        if (isPixyConnected){
+            // System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
 
-        int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 12);
+            int blockCount = m_pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 12);
         if (blockCount <= 0) {
             return null; // If blocks were not found, stop processing
         }
-        ArrayList<Block> blocks = pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
+        ArrayList<Block> blocks = m_pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
         Block largestBlock = null;
         for (Block block : blocks) { // Loops through all blocks and finds the widest one
             if (largestBlock == null) {
@@ -77,8 +108,13 @@ public class PixyCam {
         }
         return largestBlock;
     }
+        else {
+            System.out.println("Pixy isn't connected. Returning null (largestBlock()). This is a [REDACTED] moment");
+            return null;
+        }
+    }
 
-    public Pixy2 getPixy(){return pixy;}
+    public Pixy2 getPixy(){return m_pixy;}
 
 }
 
