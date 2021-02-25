02dd36aa9b61fa61eec0887a6b9ef8627054ddf8
Caleb <calebkugel1@gmail.com>
2021-02-25 17:58:28 -0600

    Revert "+360 instead"
    This reverts commit db44652a6c43e592aa92842b8ffdf12fa6e7f5b4.

--- src/main/java/frc/team3130/robot/subsystems/Chassis.java
+++ src/main/java/frc/team3130/robot/subsystems/Chassis.java
@@ -184,7 +184,7 @@ public class Chassis extends SubsystemBase {
     public Rotation2d getHeading() {
         double angle = Navx.getAngle();
         if (angle < 0) {
-            angle += 360;
+            angle = 180 - Math.abs(angle) + 180;
         }
         return Rotation2d.fromDegrees(angle);
     }
