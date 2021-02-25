3098f40f0e8c20c0e3efe31cfa5812c0f02d06f0
Caleb <calebkugel1@gmail.com>
2021-02-24 20:47:41 -0600

    fixed big dumb points

--- src/main/java/frc/team3130/robot/RobotContainer.java
+++ src/main/java/frc/team3130/robot/RobotContainer.java
@@ -159,10 +159,10 @@ public class RobotContainer {
 
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
                 List.of(new Pose2d(1, 0, new Rotation2d(0)),
-                        new Pose2d(0, -1, new Rotation2d(Math.PI/2)),
-                        new Pose2d(-1, 0, new Rotation2d(Math.PI)),
-                        new Pose2d(0, 1, new Rotation2d(Math.PI + Math.PI/2)),
-                        new Pose2d(0, 0, new Rotation2d(Math.PI*2))
+                        new Pose2d(0, -1, new Rotation2d(3*Math.PI/2)),
+                        new Pose2d(-1, 0, new Rotation2d(Math.PI/2)),
+                        new Pose2d(0, 1, new Rotation2d(0)),
+                        new Pose2d(0, 0, new Rotation2d(3*Math.PI/2))
 
                 ), config);
 
