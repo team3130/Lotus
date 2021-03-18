af2ceeb74f954914a115556b38d9df61fc9a3d69
Tomas Valdes <tvaldes60@gmail.com>
2021-03-17 20:00:28 -0500

    Testing changes

--- src/main/java/frc/team3130/robot/RobotMap.java
+++ src/main/java/frc/team3130/robot/RobotMap.java
@@ -116,7 +116,7 @@ public class RobotMap {
     public static double kAngleChange = 0;
     public static double kHoodP = 12;
     public static double kHoodI = 0.0;
-    public static double kHoodD = 15;
+    public static double kHoodD = 22;
     public static double kHoodF = 0;
 
     //Flywheel
