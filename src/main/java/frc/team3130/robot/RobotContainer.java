66984e480f1388212644d66f89ac67a0679f2bc3
Tomas Valdes <tvaldes60@gmail.com>
2021-03-31 16:00:25 -0500

    Updated paths

--- src/main/java/frc/team3130/robot/RobotContainer.java
+++ src/main/java/frc/team3130/robot/RobotContainer.java
@@ -109,7 +109,7 @@ public class RobotContainer {
 //
 //                        config);
 
-        String trajectoryJSON = "/home/lvuser/deploy/output/" + "Slalom" + ".wpilib.json";
+        String trajectoryJSON = "/home/lvuser/deploy/output/" + "Bounce" + ".wpilib.json";
         Trajectory exampleTrajectory = new Trajectory();
         try {
             Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
