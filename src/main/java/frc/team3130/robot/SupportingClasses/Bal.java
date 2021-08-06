6b38ce4d56a851890eff58fd6a2a134cbe554125
Caleb Kugel <calebkugel1@gmail.com>
2021-08-05 23:39:21 -0500

    "stable"

--- src/main/java/frc/team3130/robot/SupportingClasses/Bal.java
+++ src/main/java/frc/team3130/robot/SupportingClasses/Bal.java
@@ -6,9 +6,6 @@ public class Bal {
     //TODO: write logic if the ball has been intook
     private boolean intaken = false;
 
-    private static int nextId;
-    private final int Id;
-
     private double[] positionRel = new double[2], positionFix = new double[2];
 
     public Bal(double[] positionFix) {
@@ -18,8 +15,6 @@ public class Bal {
     public Bal(double positionFixX, double positionFixY) {
         positionFix[0] = positionFixX;
         positionFix[1] = positionFixY;
-        Id = nextId;
-        nextId++;
     }
 
     public double[] getPositionRel() {
@@ -36,10 +31,6 @@ public class Bal {
 
     public double[] getPositionFix() {
         return positionFix;
-    }
-
-    public int getId() {
-        return Id;
     }
 
     public double getDistance() {
--- src/main/java/frc/team3130/robot/SupportingClasses/balManager.java
+++ src/main/java/frc/team3130/robot/SupportingClasses/balManager.java
@@ -10,30 +10,50 @@ import java.util.*;
  * </p>
  */
 public class balManager {
-    private HashMap<Integer, Integer> idToIndex = new HashMap<Integer, Integer>();
     private ArrayList<Bal> Balls = new ArrayList<>();
+
+    private int closestIndex = -1;
 
     public void updateBalls() {}
 
+    /**
+     * <p>checks if the ball object is already in the array list and accounts for a 0.5 precision error</p>
+     * @param bal Ball objects to be added
+     */
     public void addBall(Bal bal) {
         boolean isIN = false;
         for (int looper = 0; looper < Balls.size(); looper++) {
-            if (bal.getPositionFix() == Balls.get(looper).getPositionFix() || Balls.get(looper).getPositionFix()[0] + 0.5 == bal.getPositionFix()[0] || Balls.get(looper).getPositionFix()[0] - 0.5 == bal.getPositionFix()[0] || Balls.get(looper).getPositionFix()[1] + 0.5 == bal.getPositionFix()[1] || Balls.get(looper).getPositionFix()[1] - 0.5 == bal.getPositionFix()[1]) {
+            if (bal.getPositionFix() == Balls.get(looper).getPositionFix() || Balls.get(looper).getPositionFix()[0] + 0.5 <= bal.getPositionFix()[0] && Balls.get(looper).getPositionFix()[0] - 0.5 >= bal.getPositionFix()[0] || Balls.get(looper).getPositionFix()[1] + 0.5 >= bal.getPositionFix()[1] || Balls.get(looper).getPositionFix()[1] - 0.5 <= bal.getPositionFix()[1]) {
                 isIN = true;
             }
-            else if (bal.getId() == Balls.get(looper).getId()) {
+            else if (bal == Balls.get(looper)) {
                 isIN = true;
             }
         }
         if (!isIN) {
             int index = Balls.size();
             Balls.add(bal);
-            
+
+            if (closestIndex == -1) {
+                closestIndex = index;
+            }
+            else {
+                try {
+                    if (Balls.get(closestIndex).getDistance() == bal.getDistance()) {
+                        closestIndex = index;
+                    }
+                }
+                catch (Exception exc) {
+                    // closestID not found
+                    closestIndex = -1;
+                }
+            }
         }
     }
 
+    //TODO: write logic to get closest clump of balls to maximize number you can grab
     public Bal getClosest() {
-
+        return Balls.get(closestIndex);
     }
 
     private double getDistance(double[] arr) {
