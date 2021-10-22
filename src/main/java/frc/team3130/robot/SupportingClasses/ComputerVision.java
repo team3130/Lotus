5fb21f66e0aab52c300638b25c46e680408b6fd3
Caleb <calebkugel1@gmail.com>
2021-10-21 22:37:35 -0500

    added documentation

--- src/main/java/frc/team3130/robot/SupportingClasses/ComputerVision.java
+++ src/main/java/frc/team3130/robot/SupportingClasses/ComputerVision.java
@@ -1,9 +1,21 @@
 package frc.team3130.robot.SupportingClasses;
 
+/**
+ * <p>An abstract class for computer vision classes</p>
+ * <p>
+ *     Requires the instantiation of a rotation matrix and a translation vector
+ *     has a predict method
+ * </p>
+ */
 public abstract class ComputerVision {
     protected double[][] rotation;
     protected double[] translation;
 
+    /**
+     * <p>predicts the position of the ball relative to the location of the bot</p>
+     * @param pixel pixel value given by camera
+     * @return a matrix of the relative position of the ball
+     */
     protected double[] predict(double[] pixel) {
         double[] coords = new double[2];
         coords[0] = ((pixel[0] * rotation[0][0]) + (pixel[1] * rotation[0][1])) + translation[0];
--- src/main/java/frc/team3130/robot/SupportingClasses/PosCV.java
+++ src/main/java/frc/team3130/robot/SupportingClasses/PosCV.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
@@ -16,12 +17,24 @@ import frc.team3130.robot.subsystems.Chassis;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 
 import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * <p>
+ *     Extends the ComputerVision class
+ *      This class is intended to be ran in it's own thread
+ * </p>
+ */
 public class PosCV extends ComputerVision implements Runnable{
     private final Chassis m_chassis;
     private PixyCam pixy;
     private Pose2d position;
 
+    /**
+     *<p>instantiates the PosCV class with the chassis subsystem and an instance of pixy</p>
+     * @param m_chassis chassis subsystem
+     * @param pixy pixy class
+     */
     public PosCV(Chassis m_chassis, PixyCam pixy) {
         this.m_chassis = m_chassis;
         this.pixy = pixy;
@@ -35,7 +48,13 @@ public class PosCV extends ComputerVision implements Runnable{
         translation = new double[]{216.85726298, 63.07897139};
     }
 
+    /**
+     *<p>getter for position</p>
+     * @return position on field as a Pose2d object
+     */
     public Pose2d getPos() {
+        double[] pos = {pixy.getBlocks().get(0).getX(), pixy.getBlocks().get(0).getY()};
+        position = new Pose2d(new Translation2d(predict(pos)), new Rotation2d(pixy.getBlocks().get(0).getAngle()));
         return position;
     }
 
