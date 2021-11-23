28ba4bc680b2e181d188973b281e6b68374c480c
Caleb <calebkugel1@gmail.com>
2021-11-23 14:36:10 -0600

    This is bad logic and I can't figure out why

--- src/main/java/frc/team3130/robot/SupportingClasses/Graph.java
+++ src/main/java/frc/team3130/robot/SupportingClasses/Graph.java
@@ -2,47 +2,103 @@ package frc.team3130.robot.SupportingClasses;
 
 import frc.team3130.robot.subsystems.Chassis;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.function.Function;
+import java.util.*;
 
 public class Graph {
     HashMap<Node, Integer> nodeMap;
     private Node head;
     private ArrayList<Node> nodes;
-    private ArrayList<ArrayList<Node>> Clumps;
+    private Double[][] matrix;
 
     public Graph() {
         head = new Node(Chassis.getInstance().getPosCV().getX(), Chassis.getInstance().getPosCV().getY(), PhysicalObject.bot);
         nodes.add(head);
-
     }
 
     /**
      * A method to clump nodes. This will put the clump in an arraylist of clumps.
      */
-    private void ClumpNodes(Node start) {
+    private void ClumpNodes() {
+        boolean[] visited = new boolean[nodes.size()];
+        Node temp = head;
+        visited[0] = true;
+        while (!filled(visited)) {
+            PriorityQueue<Node> adjacentNodes = adjacent(temp, visited);
+            while (!adjacentNodes.isEmpty()) {
+                Node temp2 = adjacentNodes.poll();
+                putNodeInGraph(temp, temp2, temp.getDistance(temp2));
+                temp = temp2;
+                visited[nodeMap.get(temp)] = true;
+            }
+        }
+    }
+
+    private boolean filled(boolean[] boolArray) {
+        for (boolean item : boolArray) {
+            if (!item) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void putNodeInGraph(Node toBeAdded, Node ConnectedTo, double weight) {
+        matrix[nodeMap.get(ConnectedTo)][nodeMap.get(toBeAdded)] = weight;
+        matrix[nodeMap.get(toBeAdded)][nodeMap.get(ConnectedTo)] = weight;
+    }
+
+    private void addNode(Node newNode) {
+        nodes.add(newNode);
+        resize();
+        ClumpNodes();
+    }
+
+    private void resize() {
+        matrix = Arrays.copyOf(matrix, nodes.size());
+    }
+
+    /**
+     * Returns the adjacent nodes in each sector
+     * @param start start node
+     * @param visited boolean array of visited nodes, index cooresponds to mapped value
+     * @return a priority queue of adjacent nodes
+     */
+    private PriorityQueue<Node> adjacent(Node start, boolean[] visited) {
         // each index corresponds to a sector, starting facing forward and moving 45 degrees to the right at a time
-        double[] shortest = new double[8];
+        Node[] shortest = new Node[8];
 
         // fill the array with the maximum integer value at each spot
         Arrays.fill(shortest, Integer.MAX_VALUE);
+        //TODO: add a comparator that sorts the nodes based off of their distance to start
+        PriorityQueue<Node> adjacent = new PriorityQueue<>();
 
         // iterate through nodes
         for (int nodesLooper = 0; nodesLooper < nodes.size(); nodesLooper++) {
             // make sure that we are checking a ball
             if (nodes.get(nodesLooper).getTypeOfObject() == PhysicalObject.ball) {
                 // get the ball closest in the sector and compare it to the previous closer in the sector
-                if (shortest[degressToIndex(nodes.get(nodesLooper).getRelAngle(start))] < nodes.get(nodesLooper).getDistance(start)) {
-                    // assign the sector to the new shortest distance
-                    shortest[degressToIndex(nodes.get(nodesLooper).getRelAngle(start))] = nodes.get(nodesLooper).getDistance(start);
+                if (shortest[degreesToIndex(nodes.get(nodesLooper).getRelAngle(start))].getDistance(start) < nodes.get(nodesLooper).getDistance(start)) {
+                    // assign the sector to the new shortest distance node
+                    shortest[degreesToIndex(nodes.get(nodesLooper).getRelAngle(start))] = nodes.get(nodesLooper);
                 }
             }
         }
+
+        for (int shortLooper = 0; shortLooper < shortest.length; shortLooper++) {
+            if (shortest[shortLooper].getDistance(start) < Integer.MAX_VALUE && !visited[nodeMap.get(shortest[shortLooper])]) {
+                adjacent.add(shortest[shortLooper]);
+            }
+        }
+
+        return adjacent;
     }
 
-    private int degressToIndex(double degrees) {
+    /**
+     * Returns the index of the sector based on the angle
+     * @param degrees absolute angle from the object
+     * @return the index for the array shortest in {@link #adjacent(Node, boolean[])}
+     */
+    private int degreesToIndex(double degrees) {
         if (degrees >= 0 && degrees <= 45) {
             return 0;
         }
